6fc7e325ca830368f179933c7823e7ad76185d02
Ahmadou Lo <Ahmadou Lo>
2016-11-25 16:04:19 +0000

    adding Express
    * installing Express
    * creating directory client/views
    * creating the file index.html in client/views
    * modifying server.js to adapt Express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello from node');
+//console.log('hello from node');
+var express = require('express'),
+	app		= express();
+app.get('/',function (req,res) {
+	// body...
+	res.sendfile(__dirname + '/client/views/index.html');
+});
+app.listen(3000, function () {
+	// body...
+		console.log('I\'m Listening.....');
+})
