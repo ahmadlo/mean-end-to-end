5371c89f83a3de3247afd9c7e9a491b6ee2f0369
AHMAD <ahmadou19@gmail.com>
2016-11-28 14:27:54 +0000

    preparing

--- client/js/controller/meetups-controller.js
+++ client/js/controller/meetups-controller.js
@@ -1,13 +1,17 @@
-function meetupsController($scope) {
-	// body...
-	$scope.meetupsCount=10;
-	$scope.meetups=[
-		{name:"MEAN SF fds"},
-		{name:"mean 2"}
-
-	];	
-	$scope.createMeetup=function () {
+app.controller('meetupsController',['$scope','$resource',
+	function ($scope,$resource) {
 		// body...
-		$scope.meetups.push({name:$scope.meetupName});
+		var Meetup=$resource('/api/meetups');
+		$scope.meetups=[
+			{name:"MEAN SF fds"},
+			{name:"mean 2"}
+		];	
+		$scope.createMeetup=function () {
+			// body...
+			meetup =new Meetup()
+			meetup.name=$scope.meetupName;
+			meetup.$save();
+			
+		}
 	}
-}
+])
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 //console.log('hello from node');
-var express = require('express'),
-	app		= express();
+var express 			= require('express'),
+	app					= express(),
+	bodyParser			= require('body-parser'),
+	meetupsController 	=require('./server/controller/meetups-controller');
+
+app.use(bodyParser());
 app.get('/',function (req,res) {
 	// body...
 	res.sendfile(__dirname + '/client/views/index.html');
 });
+
+app.post('/api/meetups',meetupsController.create);
+
 app.use('/js',express.static(__dirname + '/client/js'));
+
 app.listen(3000, function () {
 	// body...
 		console.log('I\'m Listening.....');
