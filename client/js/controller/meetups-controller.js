f7537912b074ea17887158bdaebdff19d2846b8b
AHMAD <ahmadou19@gmail.com>
2016-11-28 16:05:21 +0000

    final commit
    * adding mongoose to the node_modules
    * create server side
    *

--- client/js/controller/meetups-controller.js
+++ client/js/controller/meetups-controller.js
@@ -2,15 +2,18 @@ app.controller('meetupsController',['$scope','$resource',
 	function ($scope,$resource) {
 		// body...
 		var Meetup=$resource('/api/meetups');
-		$scope.meetups=[
-			{name:"MEAN SF fds"},
-			{name:"mean 2"}
-		];	
+		$scope.meetups=[];	
+		Meetup.query(function(results){
+			$scope.meetups=results;
+		});
 		$scope.createMeetup=function () {
 			// body...
 			meetup =new Meetup()
 			meetup.name=$scope.meetupName;
-			meetup.$save();
+			meetup.$save(function(result){
+				$scope.meetups.push(result);
+			});
+			$scope.meetupName='';
 			
 		}
 	}
--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 var express 			= require('express'),
 	app					= express(),
 	bodyParser			= require('body-parser'),
+	mongoose			=require('mongoose'),
 	meetupsController 	=require('./server/controller/meetups-controller');
 
+
+mongoose.connect('mongodb://localhost:27017/mean-demo')
 app.use(bodyParser());
 app.get('/',function (req,res) {
 	// body...
@@ -11,6 +14,7 @@ app.get('/',function (req,res) {
 });
 
 app.post('/api/meetups',meetupsController.create);
+app.get('/api/meetups',meetupsController.list);
 
 app.use('/js',express.static(__dirname + '/client/js'));
 
